f470a3b0ff1817da5406536f932f0d8d1a94ea91
ridhimachauhan <160222839+ridhimachauhan@users.noreply.github.com>
2024-02-16 17:54:45 +0800

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -17,14 +17,14 @@ weight = st.number_input("Enter your weight in KG", step = 0.1)
 if weight >= 1000.0 :
     print("Don't play with this")
     return weight
-else proceed 
+else print("Okay!)
 
 height = st.number_input("Enter your height in Meters", step = 0.01)
 
 if height >= 3.00 :
     print ("Oi!") 
     return height
-else proceed 
+else print("Okay!)
 
 def calculate_bmi():
     bmi = weight/(height)**2
